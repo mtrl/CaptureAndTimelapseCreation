577374ac4b8317a9b8bf5d9b809c3c31d1c9f45b
Mark Williams <mark@mtrl.co.uk>
2016-10-05 10:47:17 +0100

    Slower capture

--- capture.sh
+++ capture.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir captured
 cd captured
-watch -n20 'TIME=$(date +"%Y%m%d-%H%M%S");imagesnap -d "Mobius" $TIME.jpg'
+watch -n60 'TIME=$(date +"%Y%m%d-%H%M%S");imagesnap -d "Mobius" $TIME.jpg'
