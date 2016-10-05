7af9877d201a6c7f2b4c0a3349645cb3d43d8396
Mark Williams <mark@mtrl.co.uk>
2016-10-05 10:46:26 +0100

    Faster framerate

--- create.sh
+++ create.sh
@@ -8,7 +8,7 @@ cd ../processed
 #for i in *.jpg; do;convert $i -background white label:$i -gravity Center -append $i;done
  for i in *.jpg; do;composite label:$i -gravity south $i $i;done
 #Create the video
-ffmpeg -y -framerate 10 -pattern_type glob -i "*.jpg" -c:v libx264 -r 30 -pix_fmt yuv420p ../timelapse.mp4
+ffmpeg -y -framerate 30 -pattern_type glob -i "*.jpg" -c:v libx264 -r 30 -pix_fmt yuv420p ../timelapse.mp4
 cd ..
 # Clean up
 rm -rf processed
