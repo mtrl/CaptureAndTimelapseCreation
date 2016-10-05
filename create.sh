b025718e5f103872c0c7c9c4c0cbb2c1b5a6a2c6
Mark Williams <mark@mtrl.co.uk>
2016-10-05 10:48:57 +0100

    Create video with date as filename

--- create.sh
+++ create.sh
@@ -8,7 +8,8 @@ cd ../processed
 #for i in *.jpg; do;convert $i -background white label:$i -gravity Center -append $i;done
  for i in *.jpg; do;composite label:$i -gravity south $i $i;done
 #Create the video
-ffmpeg -y -framerate 20 -pattern_type glob -i "*.jpg" -c:v libx264 -r 30 -pix_fmt yuv420p ../timelapse.mp4
+TIME=$(date +"%Y-%m-%d")     
+ffmpeg -y -framerate 20 -pattern_type glob -i "*.jpg" -c:v libx264 -r 30 -pix_fmt yuv420p ../$TIME.mp4
 cd ..
 # Clean up
 rm -rf processed
