976c5d381d7266963288d8f8809a91768ff846d5
Mark Williams <mark@mtrl.co.uk>
2016-10-05 10:30:40 +0100

    Update for different webcam name

--- capture.sh
+++ capture.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir captured
 cd captured
-watch -n5 'TIME=$(date +"%Y%m%d-%H%M%S");imagesnap -d "Mobius #2" $TIME.jpg'
+watch -n5 'TIME=$(date +"%Y%m%d-%H%M%S");imagesnap -d "Mobius" $TIME.jpg'
