4607736c5784de85cfc19982c29a2293d049c779
Mark Williams <mark@mtrl.co.uk>
2016-10-07 13:08:32 +0100

    Changes to rotate and timestamp at capture time for nicer date formatting and to improve compat with webcam upload

--- capture.sh
+++ capture.sh
@@ -6,3 +6,8 @@ mkdir $DATE
 cd $DATE
 TIME=$(date +"%Y%m%d-%H%M%S")
 /usr/local/Cellar/imagesnap/0.2.5/bin/imagesnap -d "Mobius" $TIME.jpg
+# Rotate, timestamp and upload to mtrl
+convert $TIME.jpg -rotate 180 $TIME.jpg
+TIMESTAMP=`date`
+composite label:"$TIMESTAMP" -gravity south $TIME.jpg $TIME.jpg
+scp $TIME.jpg mark@mtrl.co.uk:www/webcam/webcam.jpg
--- create.sh
+++ create.sh
@@ -1,16 +1,5 @@
 #!/bin/zsh
-# Rotate the images
 DATE=$(date +"%Y-%m-%d")     
-mkdir processed
-cd captured/$DATE
-for i in *.jpg; do;convert $i -rotate 180 ../../processed/$i;done
-# Timestamp the images
-cd ../../processed
-#for i in *.jpg; do;convert $i -background white label:$i -gravity Center -append $i;done
- for i in *.jpg; do;composite label:$i -gravity south $i $i;done
 #Create the video
-mkdir ../videos
-ffmpeg -y -framerate 20 -pattern_type glob -i "*.jpg" -c:v libx264 -r 30 -pix_fmt yuv420p ../videos/$DATE.mp4
-cd ..
-# Clean up
-rm -rf processed
+mkdir videos
+ffmpeg -y -framerate 20 -pattern_type glob -i "captured/$DATE/*.jpg" -c:v libx264 -r 30 -pix_fmt yuv420p videos/$DATE.mp4
